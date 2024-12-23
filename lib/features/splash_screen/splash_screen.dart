655388bb8f244e6a752e4258216006afd0a921af
salma hamza <s.hamza20102002@gmail.com>
2024-12-23 20:06:25 +0200

    SA-6 Finish native splash and animated splash.

--- lib/features/splash_screen/splash_screen.dart
+++ lib/features/splash_screen/splash_screen.dart
@@ -1,10 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sabeel_app/core/theming/app_text_styles.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late Image _backgroundImage;
+  late AnimationController _controller;
+  late Animation<double> _imageScaleAnimation;
+  late Animation<double> _textSlideAnimation;
+  late Animation<double> _textOpacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _backgroundImage = Image.asset('assets/images/splash_background.jpg');
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _imageScaleAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+
+    _textSlideAnimation = Tween<double>(begin: 100.h, end: 0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+
+    _textOpacityAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    precacheImage(_backgroundImage.image, context);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: _backgroundImage.image,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedBuilder(
+                animation: _controller,
+                builder: (context, child) {
+                  return Transform.scale(
+                    scale: _imageScaleAnimation.value,
+                    child: child,
+                  );
+                },
+                child: Image.asset(
+                  'assets/images/launcher_icon-removebg-preview (2).png',
+                  height: 130.h,
+                ),
+              ),
+              AnimatedBuilder(
+                animation: _controller,
+                builder: (context, child) {
+                  return Transform.translate(
+                    offset: Offset(0, _textSlideAnimation.value),
+                    child: FadeTransition(
+                      opacity: _textOpacityAnimation,
+                      child: child,
+                    ),
+                  );
+                },
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20.h),
+                  child: Text(
+                    'طريقك إلى النور والهداية',
+                    style: AppTextStyles.font16CairoBlack
+                        .copyWith(fontWeight: FontWeight.w500),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
